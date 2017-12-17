367fed3e2e238f55405d6a3189949044c14bd310
aishwaryr <araj713@gmail.com>
2017-12-17 22:29:51 +0530

    Added styled-component, State, Filtering of results

--- js/Search.jsx
+++ js/Search.jsx
@@ -1,11 +1,39 @@
-import React from "react";
+import React, { Component } from "react";
 import ShowCard from "./ShowCard";
 import preload from "../data.json";
 
-const Search = () => (
-  <div className="search">
-    {preload.shows.map(show => <ShowCard key={show.imdbID} {...show} />)}
-  </div>
-);
+class Search extends Component {
+  state = {
+    searchTerm: ""
+  };
+  handleSearchTermChange = event => {
+    this.setState({ searchTerm: event.target.value });
+  };
+  render() {
+    return (
+      <div className="search">
+        <header>
+          <h1>ChillFlix</h1>
+          <input
+            onChange={this.handleSearchTermChange}
+            value={this.state.searchTerm}
+            type="text"
+            placeholder="Search"
+          />
+        </header>
+        <div>
+          {preload.shows
+            .filter(
+              show =>
+                `${show.title} ${show.description}`
+                  .toUpperCase()
+                  .indexOf(this.state.searchTerm.toUpperCase()) >= 0
+            )
+            .map(show => <ShowCard key={show.imdbID} {...show} />)}
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Search;
