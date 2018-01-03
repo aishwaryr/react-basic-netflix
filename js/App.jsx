175c4a6ec6aa268e61a6f289bd294c3ea0d056ff
aishwaryr <araj713@gmail.com>
2018-01-04 01:14:18 +0530

    Add Flow types; Add Header component; Finish Details view; Url parameters; conditional display of search input

--- js/App.jsx
+++ js/App.jsx
@@ -1,8 +1,13 @@
+// @flow
+
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import type { Match } from "react-router-dom";
 import Landing from "./Landing";
 import Search from "./Search";
+import Details from "./Details";
 import Data from "./Data";
+import preload from "../data.json";
 
 const FourOhFour = () => <h1>404</h1>;
 
@@ -11,7 +16,19 @@ const App = () => (
     <div className="app">
       <Switch>
         <Route exact path="/" component={Landing} />
-        <Route path="/search" component={Search} />
+        <Route
+          path="/search"
+          component={props => <Search shows={preload.shows} {...props} />}
+        />
+        <Route
+          path="/details/:id"
+          component={(props: { match: Match }) => {
+            const selectedShow = preload.shows.find(
+              show => props.match.params.id === show.imdbID
+            );
+            return <Details show={selectedShow} {...props} />;
+          }}
+        />
         <Route path="/data" component={Data} />
         <Route component={FourOhFour} />
       </Switch>
--- js/Search.jsx
+++ js/Search.jsx
@@ -1,28 +1,31 @@
+// @flow
+
 import React, { Component } from "react";
 import ShowCard from "./ShowCard";
-import preload from "../data.json";
+import Header from "./Header";
 
 class Search extends Component {
   state = {
     searchTerm: ""
   };
-  handleSearchTermChange = event => {
+  props: {
+    shows: Array<Show>
+  };
+  handleSearchTermChange = (
+    event: SyntheticKeyboardEvent & { target: HTMLInputElement }
+  ) => {
     this.setState({ searchTerm: event.target.value });
   };
   render() {
     return (
       <div className="search">
-        <header>
-          <h1>ChillFlix</h1>
-          <input
-            onChange={this.handleSearchTermChange}
-            value={this.state.searchTerm}
-            type="text"
-            placeholder="Search"
-          />
-        </header>
+        <Header
+          searchTerm={this.state.searchTerm}
+          showSearch
+          handleSearchTermChange={this.handleSearchTermChange}
+        />
         <div>
-          {preload.shows
+          {this.props.shows
             .filter(
               show =>
                 `${show.title} ${show.description}`
