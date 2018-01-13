3bc35f237649f17440f973464faa9ae6c45e6092
aishwaryr <araj713@gmail.com>
2018-01-13 14:43:06 +0530

    Add ajax call for rating in Details Comp from fake api, Add lifecycle method

--- js/ShowCard.jsx
+++ js/ShowCard.jsx
@@ -1,15 +1,18 @@
 // @flow
 
-import React from "react";
+import React, { Component } from "react";
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
-const Wrapper = styled.div`
+const Wrapper = styled(Link)`
   width: 32%;
   border: 2px solid #333;
   border-radius: 4px;
   margin-bottom: 25px;
   padding-right: 10px;
   overflow: hidden;
+  color: black;
+  text-decoration: none;
 `;
 
 const Image = styled.img`
@@ -18,23 +21,28 @@ const Image = styled.img`
   margin-right: 10px;
 `;
 
-const ShowCard = (props: {
-  poster: string,
-  title: string,
-  year: string,
-  description: string
-}) => (
-  <Wrapper>
-    <Image
-      src={`/public/img/posters/${props.poster}`}
-      alt={`${props.title} Show Poster`}
-    />
-    <div>
-      <h3>{props.title}</h3>
-      <h4>({props.year})</h4>
-      <p>{props.description}</p>
-    </div>
-  </Wrapper>
-);
+class ShowCard extends Component {
+  // This will prevent the comp from updating on render as it doesn't need to, this improves performance
+  // Will need to remove this if state is added to this comp, eg- some field is coming from API. Although condition can be added to update only when that field needs to update.
+  shouldComponentUpdate() {
+    return false;
+  }
+  props: Show;
+  render() {
+    return (
+      <Wrapper to={`/details/${this.props.imdbID}`}>
+        <Image
+          src={`/public/img/posters/${this.props.poster}`}
+          alt={`${this.props.title} Show Poster`}
+        />
+        <div>
+          <h3>{this.props.title}</h3>
+          <h4>({this.props.year})</h4>
+          <p>{this.props.description}</p>
+        </div>
+      </Wrapper>
+    );
+  }
+}
 
 export default ShowCard;
