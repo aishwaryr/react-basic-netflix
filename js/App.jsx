5dc341205f9a16eb60dd2ffb9961717721df4298
aishwaryr <araj713@gmail.com>
2018-05-23 01:01:06 +0530

    Add server side rendering

--- js/App.jsx
+++ js/App.jsx
@@ -1,7 +1,7 @@
 // @flow
 
 import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import type { Match } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store";
@@ -14,25 +14,23 @@ import preload from "../data.json";
 const FourOhFour = () => <h1>404</h1>;
 
 const App = () => (
-  <BrowserRouter>
-    <Provider store={store}>
-      <div className="app">
-        <Switch>
-          <Route exact path="/" component={Landing} />
-          <Route path="/search" component={props => <Search shows={preload.shows} {...props} />} />
-          <Route
-            path="/details/:id"
-            component={(props: { match: Match }) => {
-              const selectedShow = preload.shows.find(show => props.match.params.id === show.imdbID);
-              return <Details show={selectedShow} {...props} />;
-            }}
-          />
-          <Route path="/data" component={Data} />
-          <Route component={FourOhFour} />
-        </Switch>
-      </div>
-    </Provider>
-  </BrowserRouter>
+  <Provider store={store}>
+    <div className="app">
+      <Switch>
+        <Route exact path="/" component={Landing} />
+        <Route path="/search" component={props => <Search shows={preload.shows} {...props} />} />
+        <Route
+          path="/details/:id"
+          component={(props: { match: Match }) => {
+            const selectedShow = preload.shows.find(show => props.match.params.id === show.imdbID);
+            return <Details show={selectedShow} {...props} />;
+          }}
+        />
+        <Route path="/data" component={Data} />
+        <Route component={FourOhFour} />
+      </Switch>
+    </div>
+  </Provider>
 );
 
 export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -3,12 +3,7 @@ const webpack = require("webpack");
 
 module.exports = {
   context: __dirname,
-  entry: [
-    "react-hot-loader/patch",
-    "webpack-dev-server/client?http://localhost:8080",
-    "webpack/hot/only-dev-server",
-    "./js/ClientApp.jsx"
-  ],
+  entry: ["webpack-hot-middleware/client?path=__webpack_hmr&timeout=2000", "./js/ClientApp.jsx"],
   devtool: "cheap-eval-source-map",
   output: {
     path: path.join(__dirname, "public"),
@@ -28,10 +23,7 @@ module.exports = {
     reasons: true,
     chunks: false
   },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NamedModulesPlugin()
-  ],
+  plugins: [new webpack.HotModuleReplacementPlugin(), new webpack.NamedModulesPlugin()],
   module: {
     rules: [
       {
